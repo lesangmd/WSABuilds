d49f56efa6bbe5522370793ce7a3bbd2b93d980f
lesangmd <43851951+lesangmd@users.noreply.github.com>
2026-09-24 21:33:28 +0700

    Android v1.0.3: add immersive lifecycle method

--- .nah-iso15189-android-v1.0.1/project/app/src/main/java/vn/nah/iso15189suite/MainActivity.java
+++ .nah-iso15189-android-v1.0.1/project/app/src/main/java/vn/nah/iso15189suite/MainActivity.java
@@ -325,6 +325,40 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void applyImmersiveMode() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                getWindow().setDecorFitsSystemWindows(false);
+                WindowInsetsController controller = getWindow().getInsetsController();
+                if (controller != null) {
+                    controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                    controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                }
+            } else {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
+            }
+        } catch (Throwable ignored) {}
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        applyImmersiveMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) applyImmersiveMode();
+    }
+
     @Override
     protected void onPause() {
         CookieManager.getInstance().flush();
